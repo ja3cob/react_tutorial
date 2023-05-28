1f1520c71192bd174bf45ff42252e930567156b8
Jakub Becker <jakub.becker@banqsoft.pl>
2023-05-28 18:37:20 +0200

    Close menu on logo click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
         <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to='/' className='navbar-logo'>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                     Witaj
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
