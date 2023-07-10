30e64b172f0920a2d20edbaada222f743bbe6709
ja3cob <jakubecker@gmail.com>
2023-07-10 20:14:48 +0200

    Fix cardItem

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function CardItem(src, path, label, text) {
+const CardItem = ({src, path, label, text}) => {
   return (
     <>
     <li className='cards__item'>
         <Link className='cards__item__link' to={path}>
             <figure className='cards__item__pic-wrap' data-category={label}>
-                <img src={src} alt='Travel image' className='cards__item__img'/>
+                <img src={src} alt='Travel' className='cards__item__img'/>
             </figure>
             <div className='cards__item__info'>
                 <h5 className='cards__item__text'>{text}</h5>
