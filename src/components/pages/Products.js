a1294c529a3f1a6140b8074cff76fd0649be5ada
Jakub Becker <jakub.becker@banqsoft.pl>
2023-07-10 23:14:11 +0200

    Add pages

---
+++ src/components/pages/Products.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Products() {
+  return <h1 className='products'>Produkty</h1>
+}
+
+export default Products
---
+++ src/components/pages/Services.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Services() {
+  return <h1 className='services'>USŁUGI</h1>
+}
+
+export default Services
