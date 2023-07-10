e437663723123aa0de38177afaf2509d9281b53d
Jakub Becker <jakub.becker@banqsoft.pl>
2023-07-10 21:29:09 +0200

    Adjust cards styling, add cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,30 +7,40 @@ function Cards() {
     <div className='cards'>
         <h1>Check out epic destinations!</h1>
         <div className='cards__container'>
-            <div className='cards__wrapper'>
-                <ul className='cards__items'>
-                    <CardItem 
-                        src='images/img-9.jpg'
-                        text='Explore the hidden waterfall' 
-                        label='Adventure' 
-                        path='/services'
-                    />
-                    <CardItem 
-                        src='images/img-8.jpg'
-                        text='Explore dunes' 
-                        label='Desert' 
-                        path='/services'
-                    />
-                </ul>
-                <ul className='cards__items'>
-                    <CardItem 
-                        src='images/img-2.jpg'
-                        text='Explore islands' 
-                        label='Islands' 
-                        path='/services'
-                    />
-                </ul>
-            </div>
+            <ul className='cards__items'>
+                <CardItem 
+                    src='images/img-9.jpg'
+                    text='Explore the hidden waterfall' 
+                    label='Adventure' 
+                    path='/services'
+                />
+                <CardItem 
+                    src='images/img-8.jpg'
+                    text='Explore dunes' 
+                    label='Desert' 
+                    path='/services'
+                />
+            </ul>
+            <ul className='cards__items'>
+                <CardItem 
+                    src='images/img-2.jpg'
+                    text='Explore islands' 
+                    label='Islands' 
+                    path='/services'
+                />
+                <CardItem 
+                    src='images/img-3.jpg'
+                    text='Explore the ocean' 
+                    label='Ocean' 
+                    path='/services'
+                />
+                <CardItem 
+                    src='images/img-4.jpg'
+                    text='Explore football pitch' 
+                    label='Football' 
+                    path='/services'
+                />
+            </ul>
         </div>
     </div>
   )
